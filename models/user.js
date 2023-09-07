c72ece4d79008ec0a5fd330279dfe0f2b98b43a3
Okazaki92 <vincentslominski@gmail.com>
2023-09-08 00:56:48 +0200

    feat/added email send service

--- models/user.js
+++ models/user.js
@@ -23,6 +23,15 @@ const user = new Schema({
   avatarURL: {
     type: String,
   },
+
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, "Verify token is required"],
+  },
 });
 
 const User = mongoose.model("user", user);
